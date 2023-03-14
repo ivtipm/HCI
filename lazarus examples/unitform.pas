7c1a9138af4c3ff796404700c60f39ae336158b0
VetrovSV <eridani_alpha@hotmail.com>
2023-03-15 02:34:55 +0900

    + TStringGrid Example

---
+++ lazarus examples/unitform.pas
@@ -0,0 +1,139 @@
+unit unitForm;
+// todo: Указать везде автора программы
+// todo: документирующие комментарии для типов, переменных, процедур, функций
+// todo: поясняющие комментарии в сложных местах или в местах использования новых компонентов и свойств
+// todo: проверить корректность идентификаторов
+// todo: проверить окно:
+// - интерфейс понятный, аккуратный, соблюдены принципы близости, общей зоны и т.п.
+// - горячие клавиши
+
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Grids, Spin,
+  Arrays;  // модуль для работы с массивами
+
+type
+
+  { TFormMain }
+  // тип данных, описывающий окно программы
+  TFormMain = class(TForm)
+    //
+    Button_GenMatr: TButton;
+    CheckBox_regen: TCheckBox;
+
+    Label1: TLabel;
+    Label_help1: TLabel;
+
+    // панель с названием
+    GroupBox_FillType: TGroupBox;
+    // переключатели:
+    RadioButton_int: TRadioButton;
+    RadioButton_real: TRadioButton;
+    // пеореключатели помещены на панель GroupBox_FillType:
+    // МКМ на RadioButton_int > изменить родителя;
+    //   Далее задать положение внутри родителя через свойства Top, Left
+    // Переключатели _автоматически_ переключаются при клике на один из них,
+    // если они находятся внутри одного любого элемента
+
+    // числое поле ввода (со вкладки Misc)
+    SpinEdit_n: TSpinEdit;
+    // см. свойства: Value (значение), MinValue, MaxValue, Increment
+    // см. обработчик события OnChange
+
+
+
+    // таблица
+    StringGrid: TStringGrid;
+    // основные свойтсва:
+      // ColCount, RowCount - количество строк и столбцов
+      // DefaultColWidth, DefaultRowHeight - ширина столбца, высота строки
+      // FixedCols, FixedRows - количество фиксированных строк, столбцов
+      // (столбцы и строки в начале таблицы,более тёмного цвета)
+
+    // ОБРАБОТЧИКИ СОБЫТИЙ:
+    /// Создаёт матрицу и заполняет таблицу
+    procedure Button_GenMatrClick(Sender: TObject);
+    procedure SpinEdit_nChange(Sender: TObject);
+
+
+
+  private
+
+  public
+
+  end;
+
+var
+  FormMain: TFormMain;       // переменная, отвечающая за окно
+  matr: Matrix;              //
+
+  n,m: integer;              //
+
+
+
+implementation
+
+// директива компилятора: указание соответствующего ( $R ) файла формы
+{$R *.lfm}
+
+
+
+{ TFormMain }
+/// Создаёт матрицу и заполняет таблицу
+procedure TFormMain.Button_GenMatrClick(Sender: TObject);
+var i,j:integer;
+begin
+     // todo: SpinEdit for min, max
+     n:=5;
+     m:=5;
+
+     // если включен переключатель
+     if RadioButton_int.Checked then
+         matrix_random_int(matr, n, m, 0, 10)          // todo: Edit for min, max
+     else
+         matrix_random_real(matr, n, m, 0, 10);
+
+     // зададим количество столбцов и строк в таблице
+     StringGrid.ColCount := m;
+     StringGrid.RowCount := n;
+
+     // вывод матрицы в таблицу
+     for i:=1 to n do
+         for j:=1 to m do
+             StringGrid.Cells[i-1,j-1] := FLoatToStr( matr[i,j] );
+end;
+
+
+/// Обработчик события: изменения значения в SpinBox
+procedure TFormMain.SpinEdit_nChange(Sender: TObject);
+// обработчик создан по двойному клику
+begin
+     n := SpinEdit_n.Value;
+     // todo: изменять размер таблицы
+     // todo: аналогичный обработчик для числа столбцов
+
+     // если отмечена галочка "перегенерировать матрицу"
+     if CheckBox_regen.Checked then
+         // вызов обработчика заполнения матрицы
+         Button_GenMatrClick( Sender );
+         // имя типа (TFormMain) перед обработчиком не указывается, т.к.
+         // он вызывается внутри другого обработчика этого же окна
+         // Sender -- переменная хранящая информацию о том, кто инициировал ввзов обработчика
+
+end;
+
+end.
+
+
+{
+
+- TStringGrid
+- TSpinEdit, OnChange
+- TGroupBox, TRadioButton
+
+}
+
