aa6880190cd3eacb0e2073ff2c82dc0217f97924
VetrovSV <eridani_alpha@hotmail.com>
2026-03-24 15:12:39 +0900

    Add menu, actions, file I/O, and fix button caption
    The commit adds a main menu with file and help items, implements TActionList and associated actions
    (calc, exit, load, save), adds hotkeys and hints, updates the "Вычислить" button caption with Alt+V
    shortcut, integrates geometry module usage, and refactors UI and logic for better structure.

--- plans/2026/Examples/LazarusSimpleApp/unit_window.pas
+++ plans/2026/Examples/LazarusSimpleApp/unit_window.pas
@@ -8,7 +8,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  geometry // модуль для вычислений
+  ActnList, Menus, geometry // модуль для вычислений
   ;
 
 type
@@ -17,21 +17,48 @@ type
   { TMainForm }
   // TMainForm -- тип данных (класс), описывабщий главное окно
   TMainForm = class(TForm)
+
+    Action_calc: TAction;
+    ActionList: TActionList;
+
     // Поля класса (здесь это всё элементы интерфейса)
     Button_cacl: TButton;
     Edit_a: TEdit;       // Поле для длины основания треугольника
     Edit_h: TEdit;       // Поле для высоты
     Image: TImage;
+    Image_background: TImage;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label_s: TLabel;     // Label для вывода площади
+
     Memo_report: TMemo;
+
+    MainMenu: TMainMenu;
+    MenuItem_author: TMenuItem;
+    MenuItem_Exit: TMenuItem;
+    MenuItem_Load: TMenuItem;
+    MenuItem_Save: TMenuItem;
+    MenuItem_File: TMenuItem;
+    MenuItem_about: TMenuItem;
+    OpenDialog: TOpenDialog;
+    SaveDialog: TSaveDialog;
 
     // Обработчики событий:
     // Обработчик событий нажатие для кнопки
     procedure Button_caclClick(Sender: TObject);
+    procedure MenuItem_authorClick(Sender: TObject);
+    procedure MenuItem_ExitClick(Sender: TObject);
+    procedure MenuItem_LoadClick(Sender: TObject);
+    procedure MenuItem_SaveClick(Sender: TObject);
+
+    /// .....
+    function read_and_check_values(): boolean;
+
   private
+
+      // ...
+      a, h: Real;
 
   public
 
@@ -43,6 +70,10 @@ var
   // Переменная MainForm - окно программы
   MainForm: TMainForm;
 
+const
+  // цвет обозначающий ошибку
+  color_error = TColor( $503ed6  ); // формат цвета BGR
+  // $ обозначает что число записано в системе счисления по основания не 10, а 16
 
 
 implementation
@@ -52,46 +83,63 @@ implementation
 { TMainForm }
 
 
+/// .....
+function TMainForm.read_and_check_values(): boolean;
+var
+  is_error: boolean;
+begin
+     is_error := False;
+     Edit_a.Font.Color := clBlack;
+     Edit_h.Font.Color := clBlack;
+
+          ///
+          if not TryStrToFloat( Edit_a.Text, a ) then
+             begin
+               a := 0;
+               Memo_report.Lines.Append('Ошибка задания длины основания');
+
+               Edit_a.Font.Color := color_error;
+
+               is_error := True;
+             end;
+
+          if not TryStrToFloat( Edit_h.Text, h ) then
+             begin
+               h := 0;
+               Memo_report.Lines.Append('Ошибка задания высоты');
+               Edit_h.Font.Color := color_error;
+               is_error := True;
+             end;
+
+
+          if a < 0 then
+             begin
+                Memo_report.Lines.Append('Длина основания не может быть отрицательной');
+                Edit_a.Font.Color := color_error;
+                is_error := True;
+             end;
+
+
+          if h < 0 then
+             begin
+                Memo_report.Lines.Append('Высота не может быть отрицательной');
+                Edit_h.Font.Color := TColor( $503ed6  );
+                is_error := True;
+             end;
+
+          result := is_error;
+end;
 
 
 // Обрабочик события - нажатие на кнопку
 procedure TMainForm.Button_caclClick(Sender: TObject);
 var
   // длина оснвоания, высота, площадь трегольника
-  a, h, s : Real;
+  s : Real;
   is_error: boolean;
 begin
-     is_error := False;
 
-     ///
-     if not TryStrToFloat( Edit_a.Text, a ) then
-        begin
-          a := 0;
-          Memo_report.Lines.Append('Ошибка задания длины основания');
-          is_error := True;
-        end;
-
-     if not TryStrToFloat( Edit_h.Text, h ) then
-        begin
-          h := 0;
-          Memo_report.Lines.Append('Ошибка задания высоты');
-          is_error := True;
-        end;
-
-
-     if a < 0 then
-        begin
-           Memo_report.Lines.Append('Длина основания не может быть отрицательной');
-           is_error := True;
-        end;
-
-
-     if h < 0 then
-        begin
-           Memo_report.Lines.Append('Высота не может быть отрицательной');
-           is_error := True;
-        end;
-
+     is_error := read_and_check_values();
 
      if is_error = False then
         begin
@@ -103,6 +151,55 @@ begin
           // см. Format
      end;
 
+end;
+
+
+// Обработчик события: Пунт меню Об авторе
+procedure TMainForm.MenuItem_authorClick(Sender: TObject);
+begin
+     ShowMessage( 'Автор: Ветров С.В. ' );
+end;
+
+
+// Обработчик события: выход
+procedure TMainForm.MenuItem_ExitClick(Sender: TObject);
+begin
+     Close;
+end;
+
+
+// Обработчик события: Загрузка данных в поля ввода из файла
+procedure TMainForm.MenuItem_LoadClick(Sender: TObject);
+begin
+
+
+end;
+
+
+// Обработчик события: Сохранение данных из полей ввода в файл
+procedure TMainForm.MenuItem_SaveClick(Sender: TObject);
+var
+  filename: string;
+  file_data: Text; // файловая переменная
+begin
+
+     if read_and_check_values() = False then
+        begin
+
+         if SaveDialog.Execute then ;                       // показать окно выбора файла
+         begin
+         filename := SaveDialog.FileName;
+
+         AssignFile ( file_data, filename );
+
+         Rewrite( file_data );
+
+         writeln(file_data, a);
+         writeln(file_data, h);
+
+         CloseFile( file_data );
+         end;
+        end;
 end;
 
 end.
